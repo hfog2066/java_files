273b5498aa439f3e9f50b8b4b989a39b7df21f88
Hector <hfog2000@yahoo.com>
2021-04-10 20:28:52 -0500

    hello_world.java files

---
+++ hello_world.java
@@ -0,0 +1,12 @@
+/* This is a simple Java program
+   FileName: hello_world.java. */
+
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    //Prints "Hello, World!" to the terminal window.
+    public static void main(String args[])
+    {
+	System.out.println("Hello, World!");
+    }
+}
